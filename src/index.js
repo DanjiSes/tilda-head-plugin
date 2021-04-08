384bdfda84bf365900208f88ead61ae211db3319
Den <danil.savchenko.dev@gmail.com>
2021-04-08 15:01:32 +0600

    Finish v1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,67 @@
-alert('РОБИТ!')
+import './style.scss'
+
+const __sCTA = window.sCTA || {
+  text: 'КЛИКАЙ СЮДА',
+  link: 'https://savchenko.dev',
+  newWindow: true,
+  background: '#ffe100',
+  color: '#000',
+  ctaDelay: 3, // sec
+}
+
+$(function() {
+  const $rec = $('#rec301537474').hide()
+  const $video = $rec.find('video')
+  const [video] = $video.get()
+
+  video.muted = true
+  video.loop = true
+  video.autoplay = true
+  video.controls = false
+
+  video.play()
+
+  let ctaTimerId = null
+
+  const $widget = $(`
+    <div class="s-head">
+      <div class="s-close"></div>
+      <div class="s-minimize"></div>
+      <a class="s-cta t-text" href="${__sCTA.link}"
+         style="
+          color: ${__sCTA.color};
+          background: ${__sCTA.background};
+         "
+         ${__sCTA.newWindow ? 'target="_blank"' : ''}
+      >
+        ${__sCTA.text}
+      </a>
+    </div>
+  `)
+      .on('click', function(e) {
+        video.muted = false
+        $widget.addClass('active')
+
+        ctaTimerId = window.setTimeout(() => $widget.find('.s-cta').fadeIn(), __sCTA.ctaDelay * 1000)
+      })
+      .on('click', '.s-close', function(e) {
+        e.preventDefault()
+        e.stopPropagation()
+        $widget.remove();
+      })
+      .on('click', 'video', function(e) {
+        e.preventDefault()
+      })
+      .on('click', '.s-minimize', function(e) {
+        e.preventDefault()
+        e.stopPropagation()
+        video.muted = true
+        $widget.removeClass('active')
+        $widget.find('.s-cta').hide();
+      })
+
+  $widget.append($video)
+
+  $('body').append($widget)
+})
+
