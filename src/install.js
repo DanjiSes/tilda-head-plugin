22df5c4c0b0dec943f5896dd9a615dec90356003
Den <danil.savchenko.dev@gmail.com>
2021-05-05 01:19:51 +0600

    Add install script

---
+++ src/install.js
@@ -0,0 +1,14 @@
+$(function() {
+  const hash = '[hash]'
+  const devMode = window.location.hostname.split('.').reverse()[0] === 'ws'
+
+  const script = document.createElement('script')
+  script.type = 'text/javascript'
+  script.src = devMode ? 'http://savchenko.sl:3000/bundle.js' : 'https://danjises.github.io/tilda-head/dist/bundle.' + hash + '.js'
+
+  const link = document.createElement('link')
+  link.rel = 'stylesheet'
+  link.href = devMode ? 'http://savchenko.sl:3000/bundle.css' : 'https://danjises.github.io/tilda-head/dist/bundle.' + hash + '.css'
+
+  $('body').append([script, link])
+})
